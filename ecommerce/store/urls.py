0f500bce37a1ed9693b49b2286a3a5ca663692c2
tCruz90 <64757583+tCruz90@users.noreply.github.com>
2021-05-26 23:01:57 +0100

    added additional urls

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
 	#Leave as empty string for base url
+	path('register/', views.registerPage, name="register"),
+  path('login/', views.loginPage, name="login"),
+  path('logout/', views.logoutUser, name="logout"),
+	
 	path('', views.store, name="store"),
 	path('cart/', views.cart, name="cart"),
 	path('checkout/', views.checkout, name="checkout"),
