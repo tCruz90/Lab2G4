ce66fcfa31bc24aedd7070922e368340e6c69896
tCruz90 <64757583+tCruz90@users.noreply.github.com>
2021-05-27 22:40:19 +0100

    Update settings.py

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -146,7 +146,7 @@ STATICFILES_DIRS = [
 ]
 """
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
